5819c5d43cedea9e3d88614fb91988107ffb5bc7
Jason <foureyestrader@gmail.com>
2025-06-29 23:11:59 +0800

    finished first version of app. but it is repetitive code

--- index.js
+++ index.js
@@ -3,3 +3,22 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+let lengthDescription = document.getElementById("lengthdesc")
+let weightDescription = document.getElementById("weightdesc")
+let volumeDescription = document.getElementById("volumedesc")
+
+
+// 20 meters = 65.616 feet | 20 feet = 6.096 meters
+
+// 20 litres = 5.284 gallons | 20 gallons =75.708 litres
+
+// 20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos
+
+
+// Onclick of convert , will create 3 functions
+//need grab hold of input
+
+
+//` ${input} meters = ${input *3.281}` | ${input}  = ${input /3.281}
+// lengthDescription.textContent 
